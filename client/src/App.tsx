40f2a9c22f6a263e9e9b446e821349cd74cec161
Rohana Harsha <rohanaharsha@gmail.com>
2025-09-14 11:27:51 +0530

    tailwind installation successful

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,20 @@
 
 import { Link, Outlet } from 'react-router-dom';  
+import './App.css';
+
 
 const App = () => (
   <>
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" className='text-blue-500 hover:underline'>Home</Link>
         </li>
         <li>
-          <Link to="/GuestRegistration">Guest Registration</Link>
+          <Link to="/GuestRegistration" className='text-blue-500 hover:underline'>Guest Registration</Link>
         </li>
         <li>
-          <Link to="/GuestList">Guest List</Link>
+          <Link to="/GuestList" className='text-blue-500 hover:underline'>Guest List</Link>
         </li>
       </ul>
     </nav>
